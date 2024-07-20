5b16d936afe884108779012a03f8b7b783b9625d
nattrio <jirapat.nattrio@gmail.com>
2024-07-20 17:16:33 +0700

    docs: update swagger docs

--- app/post/docs.go
+++ app/post/docs.go
@@ -17,6 +17,8 @@ type createPostRequest struct {
 type createPostResponse struct {
 	// in: body
 	Body struct {
+		// Example: post created
+		Message string `json:"message"`
 	}
 }
 
@@ -50,4 +52,49 @@ type getPostRequest struct {
 type getPostResponse struct {
 	// in: body
 	Body Post
+}
+
+// swagger:route PATCH /posts/{id} post updatePost
+// Update a post by ID.
+// responses:
+//   200: updatePostResponse
+
+// swagger:parameters updatePost
+type updatePostRequest struct {
+	// in: path
+	ID string `json:"id"`
+
+	// in: body
+	Body CreatePost
+}
+
+// Update a post by ID successfully.
+// swagger:response updatePostResponse
+type updatePostResponse struct {
+	// in: body
+	Body struct {
+		// Example: post updated
+		Message string `json:"message"`
+	}
+}
+
+// swagger:route DELETE /posts/{id} post deletePost
+// Delete a post by ID.
+// responses:
+//   200: deletePostResponse
+
+// swagger:parameters deletePost
+type deletePostRequest struct {
+	// in: path
+	ID string `json:"id"`
+}
+
+// Delete a post by ID successfully.
+// swagger:response deletePostResponse
+type deletePostResponse struct {
+	// in: body
+	Body struct {
+		// Example: post deleted
+		Message string `json:"message"`
+	}
 }
